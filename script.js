f79e340a3cdb7b18662f9bcdbcfa90afef987491
Siddique-Ahmed <itssiddique786@gmail.com>
2024-05-17 14:34:23 -0700

    add some replies

--- script.js
+++ script.js
@@ -43,16 +43,49 @@ function handleUserMessage(userText) {
   // Set timeout for bot reply
   setTimeout(() => {
     botMessage(userText, typingLi);
-  }, 1500); // 2000 ms = 2 seconds
+  }, 1000);
 }
 
 function botMessage(userText, typingLi) {
   let botReplies = {
-    hi: "hi bhai kaise ho?",
-    hello: "hi bhai mein apki kia help kar sakta hoon?",
-    "assalam o alaikum": "walaikum assalam kaise ho?",
-    "me thik hoon aap kaise ho": "mein bhi thik hoon koi help kar sakta hoon kia apki ?",
-    "g ik help chahiye thi": "g puchein me apki help karn ki puri koshish karunga",
+    hi: "hi bhai kaise ho? me apki kia madad kar sakta hoon ?",
+    ih: "hi bhai kaise ho? me apki kia madad kar sakta hoon ?",
+    hey: "hi bhai kaise ho? me apki kia madad kar sakta hoon ?",
+    hye: "hi bhai kaise ho? me apki kia madad kar sakta hoon ?",
+    yo: "hi bhai kaise ho? me apki kia madad kar sakta hoon ?",
+    "assalam-o-alaikum": "walaikum assalam bhai kaise ho? ",
+    asslamualaikum: "walaikum assalam bhai kaise ho? ",
+    salam: "walaikum assalam bhai kaise ho? ",
+    "assalam o alaikum": "walaikum assalam bhai kaise ho? ",
+    hello: "hello bro how can i help you ?",
+    helo: "hello bro how can i help you ?",
+    hlo: "hello bro how can i help you ?",
+    hleo: "hello bro how can i help you ?",
+    helllo: "hello bro how can i help you ?",
+    heli: "hello bro how can i help you ?",
+    helli: "hello bro how can i help you ?",
+    heklo: "hello bro how can i help you ?",
+    hel: "hello bro how can i help you ?",
+    hell: "hello bro how can i help you ?",
+    "how are you": "i am fine what about you ? and how can i help you ?",
+    "how are u": "i am fine what about you ? and how can i help you ?",
+    "how r u": "i am fine what about you ? and how can i help you ?",
+    "how aer you": "i am fine what about you ? and how can i help you ?",
+    "hwo are you": "i am fine what about you ? and how can i help you ?",
+    "hwo are you": "i am fine what about you ? and how can i help you ?",
+    "kese ho": "me thik aap kaise ho ? me apki kia help kar sakta hoon ?",
+    "kse ho": "me thik aap kaise ho ? me apki kia help kar sakta hoon ?",
+    "kaise ho": "me thik aap kaise ho ? me apki kia help kar sakta hoon ?",
+    "kesy ho": "me thik aap kaise ho ? me apki kia help kar sakta hoon ?",
+    "kaisa he": "me thik aap kaise ho ? me apki kia help kar sakta hoon ?",
+    "kaisa hai": "me thik aap kaise ho ? me apki kia help kar sakta hoon ?",
+    "han mujhe kuch puchna tha ? ": "g puchiye?",
+    "haan mujhy kch pochna tha ? ": "g puchiye?",
+    "g han mujhe kuch puchna tha ? ": "g puchiye?",
+    "g haan mujhe kuch puchna tha? ": "g puchiye?",
+    "g han mujhy kuch pochna tha ? ": "g puchiye?",
+    "saylani mein admission open hain ? ": "G available hain",
+    "kon konse courses available hain ?": "g saylani mein abhi filhal ye course available hain PYTHON,WEB AND APP DEVELOPMENT, AI,FEMALE WEB AND APP DEVELOPMENT,GRAPHIC DESIGINING",
   };
 
   // Remove typing indicator
